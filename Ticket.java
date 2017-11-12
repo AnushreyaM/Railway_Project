2bb1eb31495e1893f5722e5b4dd5d8fa9ec5142f
Aakansha Vatsa <aakansha.vatsa@gmail.com>
2017-11-12 19:54:33 +0530

    Polymorphism
    different ticket classes

--- Ticket.java
+++ Ticket.java
@@ -1,11 +1,26 @@
-class Ticket
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+/*
+class OOMD
 {
-	//Aakansha: you can implement different types of tickets, like first class second class, you can choose attributes
-	
-	// refer to https://books.google.co.in/books?id=A5eZa7R17ZwC&pg=PT254&lpg=PT254&dq=attributes+for+train+oo&source=bl&ots=vhK4BsK6Ge&sig=l5jaUcMGKMa0mg7Q8Jee0pJjKAc&hl=en&sa=X&ved=0ahUKEwj6gKi0lbjXAhUKLY8KHYd1CDMQ6AEISTAH#v=onepage&q=attributes%20for%20train%20oo&f=false  
-	
-	// Pg. 235 of this link
+	public static void main (String[] args) 
+	{
+		
+		String passengerName = "Abc";
+		
+		TicketCreator tc = new TicketCreator();
+		Ticket t = tc.createTicketOnClass("Eco", passengerName);
+		System.out.println("Ticket name:" +t.name);
+		System.out.println("Ticket price:" +t.price);
+	}
+}
+*/
 
+class Ticket 
+{
+	int price;
+	String name;
 	private Train train;
 	private int ticketNumber;
 	private static int ticketCount = 0; //use this to assign ticket numbers
@@ -14,4 +29,87 @@ class Ticket
 	{
 		return this.ticketNumber;
 	}
+	Ticket(int price, String passengerName) {
+		this.price = price;
+		this.name = passengerName;
+	}
 }
+
+
+class BaseTicket 
+{
+	Ticket t;
+	public Ticket createTicket(String passengerName)
+	{
+		t = new Ticket(1000, passengerName);
+		System.out.println("called base class");
+		return t;
+	}
+}
+
+class FirstClass extends BaseTicket
+{
+	Ticket t;
+	public Ticket createTicket(String passengerName)
+	{
+		t = new Ticket(3000, passengerName);
+		System.out.println("called firstclass");
+		return t;
+	}
+}
+
+class SecondClass extends BaseTicket
+{
+	Ticket t;
+	public Ticket createTicket(String passengerName)
+	{
+		t = new Ticket(2000, passengerName);
+		System.out.println("called secondclass");
+		return t;
+	}
+	
+}
+
+class EconomyClass extends BaseTicket
+{
+	Ticket t;
+	public Ticket createTicket(String passengerName)
+	{
+		t = new Ticket(500, passengerName);
+		System.out.println("called economyclass");
+		return t;
+	}
+}
+
+
+class TicketCreator 
+{
+	Ticket ticket;
+	BaseTicket ticketType;
+	public Ticket createTicketOnClass(String className, String passengerName){
+		if(className.equalsIgnoreCase("First")) {
+			ticketType = new FirstClass();
+			
+		}
+		else if(className.equalsIgnoreCase("Second")) {
+			ticketType = new SecondClass();
+			ticket = ticketType.createTicket(passengerName);
+
+		}
+		else if(className.equalsIgnoreCase("Eco")) {
+			ticketType = new EconomyClass();
+			ticket = ticketType.createTicket(passengerName);
+
+		}else {
+			ticketType = new BaseTicket();
+			ticket = ticketType.createTicket(passengerName);
+
+		}
+		return ticket;
+	
+	}
+	
+}
+
+
+
